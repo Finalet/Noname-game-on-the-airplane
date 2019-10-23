bc05f66302d62b05e5b3b7c7a3323d3090764738
Grant Oganyan <grant1599@mail.ru>
2019-10-22 19:53:30 -0700

    Initial project upload 0.1

---
+++ Noname game 1.0/Assets/Scripts/Car.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Car : MonoBehaviour
+{
+
+    public int speedMasha;
+    public int speedGrant;
+
+    public GameObject grantCar;
+    public Text scoreText;
+    public Transform respawnPos;
+
+    int GrantScore;
+    int MashaScore;
+
+    public float force;
+
+    public GameObject toungeMasha;
+    public GameObject tounge2;
+
+    void Update()
+    {
+
+        scoreText.text = "Grant : Maria\n" + GrantScore + " : " + MashaScore;
+        /* if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            float deltaTouch = Input.GetTouch(0).deltaPosition.x;
+            Debug.Log(deltaTouch);
+
+            multiplier = Screen.width / 1000;
+
+            float x = transform.localPosition.x + deltaTouch * multiplier;
+            transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+        } */
+
+        //particleSystem.transform.localRotation = transform.rotation;
+
+
+        if (Input.GetKey(KeyCode.D) && transform.position.y >= -2)
+            transform.Rotate(Vector3.up * 7);
+        else if (Input.GetKey(KeyCode.A) && transform.position.y >= -2)
+            transform.Rotate(-Vector3.up * 7);
+
+        if (Input.GetKey(KeyCode.W) && transform.position.y >= -2)
+            transform.Translate(Vector3.forward * speedMasha * Time.deltaTime);
+            //GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speedMasha * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.S) && transform.position.y >= -2)
+            transform.Translate(-Vector3.forward * speedMasha * Time.deltaTime);
+            //GetComponent<Rigidbody>().AddRelativeForce(-Vector3.forward * speedMasha * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow) && grantCar.transform.position.y >= -2)
+            grantCar.transform.Rotate(Vector3.up * 7);
+        else if (Input.GetKey(KeyCode.LeftArrow) && grantCar.transform.position.y >= -2)
+            grantCar.transform.Rotate(-Vector3.up * 7);
+
+        if (Input.GetKey(KeyCode.UpArrow) && grantCar.transform.position.y >= -2)
+            grantCar.transform.Translate(Vector3.forward * speedGrant * Time.deltaTime);
+            //grantCar.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speedGrant * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow) && grantCar.transform.position.y >= -2)
+            grantCar.transform.Translate(-Vector3.forward * speedGrant * Time.deltaTime);
+            //grantCar.GetComponent<Rigidbody>().AddRelativeForce(-Vector3.forward * speedGrant * Time.deltaTime);
+
+
+        if (transform.position.y <= -5) {
+            transform.position = respawnPos.position;
+            transform.rotation = respawnPos.rotation;
+            GrantScore++;
+        }
+        if (grantCar.transform.position.y <= -5)
+        {
+            grantCar.transform.position = respawnPos.position;
+            grantCar.transform.rotation = respawnPos.rotation;
+            MashaScore++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y >= -2)
+            GetComponent<Animation>().Play();
+
+        if (Input.GetKeyDown(KeyCode.RightShift) && grantCar.transform.position.y >= -2)
+            grantCar.GetComponent<Animation>().Play();
+
+        //Physics.IgnoreCollision(GetComponent<Collider>(), toungeMasha.GetComponent<Collider>(), true);
+
+    }
+
+
+}
---
+++ Noname game 1.0/Assets/Scripts/SpawnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+           // RoadSpawner.instance.SpawnRoad();
+        }
+    }
+}
